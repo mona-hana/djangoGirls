de978e5a501e2059d2b5459e18b0a1a496084691
mona-hana <monadi.1990@gmail.com>
2022-04-21 12:13:45 +0430

    form

--- blog/views.py
+++ blog/views.py
@@ -40,5 +40,5 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_editz.html', {'form': form})
     
